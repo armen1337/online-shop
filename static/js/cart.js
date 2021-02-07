398b2f20ce25a36ee1e13632a28ad1866aab3eef
Armen <armen14102003@mail.ru>
2021-02-07 21:01:08 +0400

    Added 'addCookieItem' function

--- static/js/cart.js
+++ static/js/cart.js
@@ -2,8 +2,8 @@ var updateBtns = document.getElementsByClassName('update-cart')
 
 for (i = 0; i < updateBtns.length; i++) {
 	updateBtns[i].addEventListener('click', function(){
-		var productId = this.dataset.product
-		var action = this.dataset.action
+		var productId = this.dataset.product  // data-product
+		var action = this.dataset.action  // data-action
 		console.log('productId:', productId, 'Action:', action)
 		console.log('USER:', user)
 
@@ -13,6 +13,32 @@ for (i = 0; i < updateBtns.length; i++) {
 			updateUserOrder(productId, action)
 		}
 	})
+}
+
+function addCookieItem(productId, action){
+	console.log('User is not authenticated')
+
+	if (action == 'add'){
+		if (cart[productId] == undefined){
+		cart[productId] = {'quantity':1}
+
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if (cart[productId]['quantity'] <= 0){
+			console.log('Item should be deleted')
+			delete cart[productId];
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+
+	location.reload()
 }
 
 function updateUserOrder(productId, action){
@@ -29,9 +55,15 @@ function updateUserOrder(productId, action){
 			body:JSON.stringify({'productId':productId, 'action':action})
 		})
 		.then((response) => {
+			if(response.ok) {
+				console.log("Success");
+			}else {
+				console.log("Not success");
+			}
 		   return response.json();
 		})
 		.then((data) => {
+			console.log("data:", data);
 		    location.reload()
 		});
 }
